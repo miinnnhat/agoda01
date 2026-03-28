17752b31e6d9ae6f710b35693519fa42eb614471
miinnnhat <131011057+miinnnhat@users.noreply.github.com>
2026-03-28 14:33:37 +0700

    Create a Open Fixture
    Adding Open Agoda link fixture

---
+++ fixture/fixture.js
@@ -0,0 +1,24 @@
+const base = require('@playwright/test');
+
+const test = base.test.extend({
+
+    agodaUrl: ['https://www.agoda.com/', { option: true }],
+
+    agodaPage: async ({ page, agodaUrl }, use) => {
+    
+    console.log(`Open Agoda: ${agodaUrl}`);
+    await page.goto(agodaUrl);
+    
+    // await page.waitForLoadState('networkidle');
+    
+    
+    await use(page);
+    //teardown 
+
+    
+    
+    }
+});
+
+export { test, base };
+
--- tests/homeSearch.spec.js
+++ tests/homeSearch.spec.js
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import {test, expect} from '../fixture/fixture.js';
 import testData from '../data/testData.json';
 import {HomePage} from '../pages/homePage.pom.js';
 import { time } from 'node:console';
@@ -7,15 +7,14 @@ import {getDate} from '../utils/dateFunction.js';
 
 test.describe("Home Search", ()=>{
     
-    test(' 01 Search functionality', async ({page}) =>{
+    test(' 01 Search functionality', async ({agodaPage}) =>{
         
-        await page.goto("https://www.agoda.com/");
-        //fill input
-        await page.locator('[data-selenium="textInput"]').pressSequentially(testData[0].hotelName, {delay: 100});
+             //fill input
+        await agodaPage.locator('[data-selenium="textInput"]').pressSequentially(testData[0].hotelName, {delay: 100});
 
         //handle popup auto complelte
-        await page.waitForSelector('[data-selenium="autoCompleteItem"]', {state: 'visible'})
-        await page.click('[data-selenium="autoCompleteItem"]') //click first item
+        await agodaPage.waitForSelector('[data-selenium="autoCompleteItem"]', {state: 'visible'})
+        await agodaPage.click('[data-selenium="autoCompleteItem"]') //click first item
 
         //click fist options
                 
@@ -23,13 +22,13 @@ test.describe("Home Search", ()=>{
     
     });
    
-    test(' 02 Date picker ', async ({page}) =>{
-        await page.goto("https://www.agoda.com/");
+    test(' 02 Date picker ', async ({agodaPage}) =>{
+        // await agodaPage.goto("https://www.agoda.com/");
 
         //opening date check in picker
-        await page.click('[data-selenium="checkInBox"]')
+        await agodaPage.click('[data-selenium="checkInBox"]')
         
-        await page.waitForTimeout(3000)
+                //await agodaPage.waitForTimeout(2000)
         
         //Validate check in date is true
         const today = new Date();
@@ -37,48 +36,48 @@ test.describe("Home Search", ()=>{
         const todayDate = today.getDate();
         while(true){
             
-            const currentMonthYear = (await page.locator('div.DayPicker-Caption').first().textContent())?.trim();
-            // const currentMonthYear = await page.locator('DayPicker-Caption').textContent();
+            const currentMonthYear = (await agodaPage.locator('div.DayPicker-Caption').first().textContent())?.trim();
+            // const currentMonthYear = await agodaPage.locator('DayPicker-Caption').textContent();
             if(currentMonthYear == todayMonthYear){
                 break;
             }
 
             //Verify last month first
-            await page.getByRole('button', { name: 'Previous Month' }).click()  //last month button
-            await page.waitForTimeout(3000)
+            await agodaPage.getByRole('button', { name: 'Previous Month' }).click()  //last month button
+                        //await agodaPage.waitForTimeout(2000)
 
             // Bạn định nghĩa ngày muốn chọn theo đúng format của thuộc tính
             const startDate = '2026-03-29';
             const endDate = '2026-03-30';
 
             // Playwright sẽ tìm thẳng thẻ span có chứa data-selenium-date tương ứng và click
-            await page.locator(`[data-selenium-date="${startDate}"]`).click();
-            await page.locator(`[data-selenium-date="${endDate}"]`).click();
+            await agodaPage.locator(`[data-selenium-date="${startDate}"]`).click();
+            await agodaPage.locator(`[data-selenium-date="${endDate}"]`).click();
 
-            await page.mouse.click(10, 10);
+            await agodaPage.mouse.click(10, 10);
 
-            await page.waitForTimeout(2000)
+            await agodaPage.waitForTimeout(2000)
 
             //Verify next month
-            await page.getByRole('button', { name: 'Next Month' }).click()  //next month button
-            await page.waitForTimeout(3000)
+            await agodaPage.getByRole('button', { name: 'Next Month' }).click()  //next month button
+            await agodaPage.waitForTimeout(3000)
 
-            await page.mouse.click(10, 10);
+            await agodaPage.mouse.click(10, 10);
 
         }
 
 
 
         //opening date check out picker
-        await page.click('[data-selenium="checkOutBox"]')
+        await agodaPage.click('[data-selenium="checkOutBox"]')
 
-        await page.waitForTimeout(2000)
+        await agodaPage.waitForTimeout(2000)
 
 
     });
 
-    test('03 Occupancy selector', async ({page}) =>{
-        await page.goto("https://www.agoda.com/");
+    test('03 Occupancy selector', async ({agodaPage}) =>{
+        // await agodaPage.goto("https://www.agoda.com/");
 
         //Opening occupancy selector
         await page.click('[data-selenium="occupancy-box"]') //if needed 
