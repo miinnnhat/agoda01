5613255770bc8864c63c318c5fa5e02619bada8a
miinnnhat <131011057+miinnnhat@users.noreply.github.com>
2026-03-29 21:38:21 +0700

    Cover flaky test

--- pages/hotelPage.pom.js
+++ pages/hotelPage.pom.js
@@ -8,9 +8,11 @@ export class HotelDetailPage {
 
     async isPriceVisible() {
         console.log("6.1 Waiting for price to be visible on the new tab");
-       
-        await this.page.mouse.wheel(0, 1000);
-        await this.page.waitForTimeout(1000);
+        
+        await this.page.bringToFront();
+        await this.page.waitForLoadState('domcontentloaded');
+        await this.page.evaluate(() => window.scrollBy(0, 1500));
+        await this.page.waitForTimeout(1500);
 
         await this.priceDisplay.waitFor({ state: 'visible', timeout: 30000 });
         return await this.priceDisplay.isVisible();
